0688794f3d582653b14f174886bc7702fff35d80
ArchiNure <mihrannuredin@gmail.com>
2024-05-14 12:02:36 -0300

    feat: :sparkles: mayor minor logic created

--- assets/js/mayorEtc.js
+++ assets/js/mayorEtc.js
@@ -1,9 +1,9 @@
 function hello(num1, num2) {
-    if (num1 < num2) {
-        console.log(`${num2} is bigger.`)
-    } else {
-        console.log(`${num1} is bigger`)
-    }
+  if (num1 < num2) {
+    console.log(`${num2} is bigger.`);
+  } else if (num1 == num2) {
+    console.log(`${num1} and ${num2} are the same.`);
+  } else {
+    console.log(`${num1} is bigger`);
+  }
 }
-
-console.log(document.body.innerHTML)
