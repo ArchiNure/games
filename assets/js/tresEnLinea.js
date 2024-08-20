8e59a7f9c5b07f314432b10f89a5d4a180f1d4e3
ArchiNure <mihrannuredin@gmail.com>
2024-08-20 13:05:55 -0300

    build: :building_construction: added changes between X and O

--- assets/js/tresEnLinea.js
+++ assets/js/tresEnLinea.js
@@ -1,9 +1,15 @@
-const box0 = document.getElementById("box0");
+let playerOneTurn = true;
 
 for (let index = 0; index < 9; index++) {
   const box = document.getElementById(`box${index}`);
 
   box.addEventListener("click", function (event) {
-    console.log("click");
+    if (playerOneTurn == true) {
+      box.classList.add("bi-cross");
+      playerOneTurn = false;
+    } else {
+      box.classList.add("bi-circle");
+      playerOneTurn = true;
+    }
   });
 }
